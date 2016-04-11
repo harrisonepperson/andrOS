9479d37b1b6dc923cf48f1a2d3a51036f09860d1
harrisonepperson <harrisonepperson@gmail.com>
2016-04-10 20:54:29 -0400

    dock sits behind windows until the bottom of the screen is touched

--- andrOS/config/initializers/omniauth.rb
+++ andrOS/config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"],
-           scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
+           scope: 'profile', image_aspect_ratio: 'square', image_size: 256, access_type: 'online', name: 'google'
 end
